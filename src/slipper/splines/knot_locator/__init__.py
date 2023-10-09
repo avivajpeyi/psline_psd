55db1de7d18ec07a0d4f8416fa09e43f95ce7586
avi <avi.vajpeyi@gmail.com>
2023-10-09 16:13:01 +1300

    refactor knot-locator

---
+++ src/slipper/splines/knot_locator/__init__.py
@@ -0,0 +1,18 @@
+import inspect
+
+from .methods import _KNOT_LOCATOR_FUNC_DICT, KnotLocatorType
+
+
+def knot_locator(knot_locator_type: KnotLocatorType, **knots_kwargs):
+    """Returns the knots for the given knot locator type and kwargs"""
+    knot_loc_func = _KNOT_LOCATOR_FUNC_DICT[knot_locator_type]
+    expected_args = inspect.getfullargspec(knot_loc_func).args
+    kwargs = {k: knots_kwargs.get(k, None) for k in expected_args}
+
+    # if any of the args are None, raise an error
+    if any([v is None for v in kwargs.values()]):
+        raise ValueError(
+            f"Missing arguments for {knot_locator_type}:\n{kwargs}"
+        )
+
+    return knot_loc_func(**kwargs)
