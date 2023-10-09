67453a01bf5151031eb49e89731016d5b5446be8
avi <avi.vajpeyi@gmail.com>
2023-10-09 14:02:18 +1300

    remove tau from log-pspline sampler

--- src/slipper/sample/log_pspline_sampler/bayesian_functions.py
+++ src/slipper/sample/log_pspline_sampler/bayesian_functions.py
@@ -7,12 +7,11 @@ def _wPw(w, P):
     return np.dot(np.dot(w.T, P), w)
 
 
-def lprior(k, w, τ, φ, φα, φβ, δ, δα, δβ, P):
+def lprior(k, w, φ, φα, φβ, δ, δα, δβ, P):
     wTPw = _wPw(w, P)
     log_prior = k * 0.5 * np.log(φ) - 0.5 * φ * wTPw
     log_prior += gamma.logpdf(φ, a=φα, scale=1 / (δ * φβ))
     log_prior += gamma.logpdf(δ, a=δα, scale=1 / δβ)
-    # log_prior += norm.logpdflogpdf(τ, 0, 100)
     return log_prior
 
 
@@ -30,21 +29,16 @@ def δ_prior(φ, φα, φβ, δα, δβ):
     return Gamma(k=shape, theta=1 / rate)
 
 
-def sample_φδ(k, w, τ, τα, τβ, φ, φα, φβ, δ, δα, δβ, data, spline_model):
+def sample_φδ(k, w, φα, φβ, δ, δα, δβ, data, spline_model):
     φ = φ_prior(k, w, spline_model.penalty_matrix, φα, φβ, δ).sample().flat[0]
     δ = δ_prior(φ, φα, φβ, δα, δβ).sample().flat[0]
     return φ, δ
 
 
-def llike(w, τ, data, spline_model):
+def llike(w, data, spline_model):
     """Whittle log likelihood"""
-    # TODO: Move to using bilby likelihood
-    # TODO: the parameters to this function should
-    #  be the sampling parameters, not the matrix itself!
-    # todo: V should be computed in here
 
     n = len(data)
-    # _lnspline = spline_model(weights=w, n=n) + τ
     _lnspline = spline_model(weights=w, n=n)
 
     is_even = n % 2 == 0
@@ -65,92 +59,12 @@ def llike(w, τ, data, spline_model):
     return lnlike
 
 
-def lpost(k, w, τ, τα, τβ, φ, φα, φβ, δ, δα, δβ, data, psline_model):
-    logprior = lprior(
-        k, w, τ, φ, φα, φβ, δ, δα, δβ, psline_model.penalty_matrix
-    )
-    loglike = llike(w, τ, data, psline_model)
+def lpost(k, w, φ, φα, φβ, δ, δα, δβ, data, psline_model):
+    logprior = lprior(k, w, φ, φα, φβ, δ, δα, δβ, psline_model.penalty_matrix)
+    loglike = llike(w, data, psline_model)
     logpost = logprior + loglike
     if not np.isfinite(logpost):
         raise ValueError(
             f"logpost is not finite: lnpri{logprior}, lnlike{loglike}, lnpost{logpost}"
         )
     return logpost
-
-
-#
-# #' log-prior
-# #' @keywords internal
-# lprior = function (k, v, tau, tau.alpha, tau.beta, phi, phi.alpha, phi.beta,
-#                    delta, delta.alpha, delta.beta, P)
-# {
-#   # Sigma^(-1) = P
-#
-#   logprior <- k * log(phi)/2 - phi * t(v) %*% P %*% v / 2 + #MNormal on weights
-#
-#     dgamma(phi, phi.alpha, delta * phi.beta, log = TRUE) + # log prior for phi
-#
-#     dgamma(delta, delta.alpha, delta.beta, log = TRUE) + # log prior for delta
-#
-#     dnorm(tau, 0, 100, log = TRUE); # prior for tau
-#
-#   return(logprior)
-# }
-#
-# #' log Whittle likelihood
-# #' @importFrom Rcpp evalCpp
-# #' @useDynLib psplinePsd, .registration = TRUE
-# #' @keywords internal
-# llike = function (omega, FZ, k, v, tau, tau.alpha, pdgrm, degree, db.list,
-#                   spec_ar)
-# {
-#   n <- length(FZ);
-#
-#   # Which boundary frequencies to remove from likelihood computation
-#   if (n %% 2) {  # Odd length time series
-#     bFreq <- 1  # Remove first
-#   }
-#   else {  # Even length time series
-#     bFreq <- c(1, n)  # Remove first and last
-#   }
-#
-#   # Un-normalised PSD (defined on [0, 1])
-#   qq.psd <- qpsd(omega, k, v, degree, db.list);
-#
-#   q = unrollPsd(qq.psd, n); # Unrolls the unnormalised PSD to length n
-#
-#   # Normalised PSD (defined on [0, pi])
-#   f <- q + tau; # CHANGED --> this is actually log-tau, and log-f
-#   f <- f + spec_ar; # Correction spec_ar is the parametric
-#
-#   # Whittle log-likelihood
-#   #llike <- -sum(log(f[-bFreq]) + pdgrm[-bFreq] / (f[-bFreq] * 2 * pi)) / 2; # original
-#   #llike <- -sum(f[-bFreq] + pdgrm[-bFreq] / (exp(f[-bFreq]) * 2 * pi)) / 2;
-#   llike <- -sum(f[-bFreq] +
-#                 exp(log(pdgrm[-bFreq]/(2 * pi)) - f[-bFreq]) ) / 2;
-#
-#   return(llike)
-#
-# }
-#
-# #' Unnormalised log posterior
-# #' @keywords internal
-# lpost = function (omega, FZ, k, v, tau, tau.alpha, tau.beta,
-#                   phi, phi.alpha, phi.beta, delta, delta.alpha, delta.beta,
-#                   P, pdgrm, degree, db.list,
-#                   spec_ar)
-# {
-#
-#   ll  <- llike(omega, FZ, k, v, tau, tau.alpha, pdgrm, degree, db.list, spec_ar)
-#
-#   lpr <- lprior(k, v, tau, tau.alpha, tau.beta, phi,
-#                 phi.alpha, phi.beta, delta, delta.alpha, delta.beta, P)
-#
-#   #print(ll)
-#   #print(lpr)
-#
-#   lp <- ll + lpr;
-#
-#   #return(lp)
-#   return(list(ll = ll, lp = lp));
-# }
--- src/slipper/sample/log_pspline_sampler/log_pspline_sampler.py
+++ src/slipper/sample/log_pspline_sampler/log_pspline_sampler.py
@@ -4,7 +4,7 @@ from typing import Callable
 import numpy as np
 
 from slipper.sample.base_sampler import BaseSampler, _update_weights
-from slipper.splines.initialisation import knot_locator
+from slipper.splines import knot_locator
 from slipper.splines.p_splines import PSplines
 
 from .bayesian_functions import lpost, sample_φδ
@@ -14,9 +14,6 @@ LnlArgs = namedtuple(
     [
         "n_basis",
         "w",
-        "τ",
-        "τα",
-        "τβ",
         "φ",
         "φα",
         "φβ",
@@ -48,7 +45,6 @@ class LogPsplineSampler(BaseSampler):
             w=np.zeros((self.n_steps, self.n_basis)),
             φ=np.zeros(self.n_steps),
             δ=np.zeros(self.n_steps),
-            τ=np.zeros(self.n_steps),
             # proposal values
             proposal_sigma=np.zeros(self.n_steps),
             acceptance_fraction=np.zeros(self.n_steps),
@@ -57,7 +53,6 @@ class LogPsplineSampler(BaseSampler):
         )
 
         sk = self.sampler_kwargs
-        self.samples["τ"][0] = 1 / (2 * np.pi)
         self.samples["δ"][0] = sk["δα"] / sk["δβ"]
         self.samples["φ"][0] = sk["φα"] / (sk["φβ"] * self.samples["δ"][0])
         self.samples["w"][0, :] = self.spline_model.guess_weights(
@@ -65,16 +60,11 @@ class LogPsplineSampler(BaseSampler):
         ).ravel()
         self.samples["proposal_sigma"][0] = 1
         self.samples["acceptance_fraction"][0] = 0.4
-        self.samples["sigma_tau"][0] = 1
-        self.samples["acceptance_fraction_tau"][0] = 0.4
         self.samples["lpost_trace"] = np.zeros(self.n_steps)
 
         self.args = LnlArgs(
             n_basis=self.n_basis,
             w=self.samples["w"][0],
-            τ=self.samples["τ"][0],
-            τα=self.sampler_kwargs["τα"],
-            τβ=self.sampler_kwargs["τβ"],
             φ=self.samples["φ"][0],
             φα=self.sampler_kwargs["φα"],
             φβ=self.sampler_kwargs["φβ"],
@@ -91,12 +81,9 @@ class LogPsplineSampler(BaseSampler):
         np.random.shuffle(aux)
         accept_frac = self.samples["acceptance_fraction"][itr - 1]
         sigma = self.samples["proposal_sigma"][itr - 1]
-        accept_frac_tau = self.samples["acceptance_fraction_tau"][itr - 1]
-        sigma_tau = self.samples["sigma_tau"][itr - 1]
 
         self.args = self.args._replace(
             w=self.samples["w"][itr - 1, :],
-            τ=self.samples["τ"][itr - 1],
             φ=self.samples["φ"][itr - 1],
             δ=self.samples["δ"][itr - 1],
         )
@@ -106,20 +93,14 @@ class LogPsplineSampler(BaseSampler):
             # 1. explore the parameter space for new V
             (
                 w,
-                τ,
                 accept_frac,
-                accept_frac_tau,
                 sigma,
-                sigma_tau,
                 lpost_store,
             ) = _tune_proposal_distribution(
                 aux,
                 accept_frac,
-                accept_frac_tau,
                 sigma,
-                sigma_tau,
                 self.args.w,
-                self.args.τ,
                 lpost_store,
                 self.args,
                 lpost,
@@ -127,28 +108,22 @@ class LogPsplineSampler(BaseSampler):
 
             # 2. sample new values for φ, δ, τ
             φ, δ = sample_φδ(*self.args)
-            self.args = self.args._replace(w=w, τ=τ, φ=φ, δ=δ)
+            self.args = self.args._replace(w=w, φ=φ, δ=δ)
 
         # 3. store the new values
         self.samples["φ"][itr] = φ
         self.samples["δ"][itr] = δ
-        self.samples["τ"][itr] = τ
         self.samples["w"][itr, :] = w
         self.samples["proposal_sigma"][itr] = sigma
         self.samples["acceptance_fraction"][itr] = accept_frac
         self.samples["lpost_trace"][itr] = lpost_store
-        self.samples["sigma_tau"][itr] = sigma_tau
-        self.samples["acceptance_fraction_tau"][itr] = accept_frac_tau
 
 
 def _tune_proposal_distribution(
     aux: np.array,
     accept_frac: float,
-    accept_frac_tau,
     sigma: float,
-    sigma_tau: float,
     weight: np.array,
-    τ,
     lpost_store,
     args,
     lnpost_fn: Callable,
@@ -161,13 +136,7 @@ def _tune_proposal_distribution(
     elif accept_frac > 0.50:  # decreasing acceptance pbb
         sigma = sigma * 1.1  # increasing proposal moves
 
-    if accept_frac_tau < 0.30:  # increasing acceptance pbb
-        sigma_tau = sigma_tau * 0.90  # decreasing proposal moves
-    elif accept_frac_tau > 0.50:  # decreasing acceptance pbb
-        sigma_tau = sigma_tau * 1.1  # increasing proposal moves
-
     accept_count = 0  # ACCEPTANCE PROBABILITY
-    accept_count_tau = 0
 
     # Update weights
     for g in range(0, n_weight_columns):
@@ -175,32 +144,10 @@ def _tune_proposal_distribution(
         weight[pos], lpost_store, accept_count = _update_weights(
             sigma, weight[pos], pos, args, lpost_store, accept_count, lnpost_fn
         )
-        # τ, lpost_store, accept_count_tau = _update_tau(
-        #     sigma_tau, τ, args, lpost_store, accept_count_tau, lnpost_fn
-        # )
-
     accept_frac = accept_count / n_weight_columns
-    accept_frac_tau = accept_count_tau / n_weight_columns
     return (
         weight,
-        τ,
         accept_frac,
-        accept_frac_tau,
         sigma,
-        sigma_tau,
         lpost_store,
-    )  # return updated values
-
-
-def _update_tau(sigma, τ, lpost_args, lpost_store, accept_count, lnpost_fn):
-    Z = np.random.normal()
-    U = np.log(np.random.uniform())
-
-    τ_star = τ + sigma * Z
-    lpost_args = lpost_args._replace(τ=τ_star)
-    lpost_star = lnpost_fn(*lpost_args)
-
-    lnl_diff = (lpost_star - lpost_store).ravel()[0]
-    if U < np.min([0, lnl_diff]):
-        return τ_star, lpost_star, accept_count + 1
-    return τ, lpost_store, accept_count
+    )
