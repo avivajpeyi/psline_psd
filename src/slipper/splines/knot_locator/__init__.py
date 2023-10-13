0efb1e39c3c5ef75fa4b545cad70ade35a3ca144
avi <avi.vajpeyi@gmail.com>
2023-10-13 17:19:00 +1300

    Add knot edge warning (has to include 0 1)

--- src/slipper/splines/knot_locator/__init__.py
+++ src/slipper/splines/knot_locator/__init__.py
@@ -3,6 +3,8 @@ from typing import Dict, Union
 
 import numpy as np
 
+from slipper.logger import logger
+
 from .methods import _KNOT_LOCATOR_FUNC_DICT, KnotLocatorType
 
 
@@ -20,4 +22,15 @@ def knot_locator(
             f"Missing arguments for {knot_locator_type}:\n{kwargs}"
         )
 
-    return knot_loc_func(**kwargs)
+    knots = knot_loc_func(**kwargs)
+
+    if knots[0] != 0 or knots[-1] != 1:
+        logger.warning(
+            "Knots must start at 0 and end at 1. "
+            "Manually changing the first and last knots to 0 and 1 respectively."
+            "Original knots:\n"
+            f"{knots}"
+        )
+        knots[0] = 0
+        knots[-1] = 1
+    return knots
