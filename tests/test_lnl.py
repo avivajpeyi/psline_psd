9ac72a7b68a538505112f2d0fbd525711ac3528d
avi <avi.vajpeyi@gmail.com>
2023-10-20 16:39:03 +1300

    Add test for LnL (failing atm)

--- tests/test_lnl.py
+++ tests/test_lnl.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from slipper.example_datasets.lisa_data import lisa_noise_periodogram
@@ -10,7 +11,6 @@ from slipper.splines.p_splines import PSplines
 
 def test_lisa_noise_lnl():
     pdgrm = lisa_noise_periodogram()
-    # keep every 5th point to speed up analysis for testing
     pdgrm = pdgrm[::5]
 
     pspline = PSplines.from_kwarg_dict(
@@ -18,12 +18,25 @@ def test_lisa_noise_lnl():
             degree=3,
             diffMatrixOrder=2,
             logged=True,
-            n_grid_points=500,
-            knot_locator_type=KnotLocatorType.data_peak,
+            n_grid_points=5000,
+            knot_locator_type=KnotLocatorType.binned_knots,
+            data_bin_edges=[10**-3, 10**-2.5, 10**-2, 0.1, 0.5],
+            data_bin_weights=[0.1, 0.3, 0.4, 0.2, 0.2, 0.1],
+            log_data=True,
             data=pdgrm,
             n_knots=30,
         )
     )
-    assert np.isfinite(
-        pspline.lnlikelihood(pdgrm, pspline.guess_weights(pdgrm))
-    )
+    w = pspline.guess_weights(pdgrm)
+    w = np.zeros_like(w)
+    w[4] = -1
+
+    model = pspline(weights=w, return_log_value=True)
+    model_x = np.linspace(0, 1, len(model))
+    fig = plt.scatter(model_x, model, s=1)
+    # x = np.linspace(0, 1, len(pdgrm))
+    # plt.scatter(x, pdgrm, s=1)
+    #
+    plt.show()
+
+    assert np.isfinite(pspline.lnlikelihood(pdgrm, weights=w))
